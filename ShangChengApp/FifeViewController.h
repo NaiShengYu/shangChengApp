16d69c79e4c8bc979b3c2db97eb694cad179c609
俞乃胜 <2495024529@qq.com>
2016-12-06 15:34:28 +0800

    增加了FifeViewController

---
+++ ShangChengApp/FifeViewController.h
@@ -0,0 +1,13 @@
+//
+//  FifeViewController.h
+//  ShangChengApp
+//
+//  Created by 俞乃胜 on 16/12/6.
+//  Copyright © 2016年 俞乃胜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FifeViewController : UIViewController
+
+@end
